0d344f1447b96ec1ee5b452baa050b2fe864d1fe
BidonBey <bidonbey@domain.com>
2025-08-06 07:47:39 +0300

    feat : create contact page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import admin
-from .views import index
+from .views import index,contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact',contact, name='contact'),
+
 ]
