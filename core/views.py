0afcac487cc440e5eaba018e97517059d8224f18
BidonBey <bidonbey@domain.com>
2025-08-31 12:16:42 +0300

    refactor : add images from DB

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import GeneralSetting
+from core.models import GeneralSetting,ImageSetting
 # Create your views here.
 
 def index(request):
@@ -11,6 +11,12 @@ def index(request):
     home_banner_title=GeneralSetting.objects.get(name='home_banner_title').parameters
     about_myself_welcome=GeneralSetting.objects.get(name='about_myself_welcome').parameters
     about_myself_footer=GeneralSetting.objects.get(name='about_myself_footer').parameters
+
+    header_logo	= ImageSetting.objects.get(name='header_logo').file
+    home_banner_image = ImageSetting.objects.get(name='home_banner_image').file
+    site_favicon = ImageSetting.objects.get(name='site_favicon').file
+
+
     context = {
         'site_title': site_title,
         'site_description': site_description,
@@ -20,18 +26,11 @@ def index(request):
         'home_banner_title': home_banner_title,
         'about_myself_welcome': about_myself_welcome,
         'about_myself_footer': about_myself_footer,
+        'header_logo': header_logo,
+        'home_banner_image': home_banner_image,
+        'site_favicon': site_favicon,
     }
 
-    context = {
-        'site_title':site_title,
-        'site_description':site_description,
-        'site_keywords':site_keywords,
-        'home_banner_name':home_banner_name,
-        'home_banner_description':home_banner_description,
-        'home_banner_title':home_banner_title,
-        'about_myself_welcome':about_myself_welcome,
-        'about_myself_footer':about_myself_footer,
-    }
 
     return render(request, 'index.html',context=context)
 
