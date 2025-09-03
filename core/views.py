f9b04f9d8397f256d19b0fb5eec41ecb0e5b2feb
BidonBey <bidonbey@domain.com>
2025-09-03 12:37:18 +0300

    feat : create document model

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from core.models import GeneralSetting, ImageSetting, Skill, Experience, Education, SocialMedia
+from django.shortcuts import render,redirect,get_object_or_404
+from core.models import GeneralSetting, ImageSetting, Skill, Experience, Education, SocialMedia, Document
 
 
 # Create your views here.
@@ -26,7 +26,8 @@ def index(request):
     educations = Education.objects.all().order_by('-start_date')
     #Social Medias
     social_medias = SocialMedia.objects.all()
-
+    #Documents
+    documents = Document.objects.all()
 
 
     context = {
@@ -45,7 +46,11 @@ def index(request):
         'experiences': experiences,
         'educations': educations,
         'social_medias': social_medias,
+        'documents': documents,
     }
 
     return render(request, 'index.html',context=context)
 
+def redirect_urls(request,slug):
+    doc = get_object_or_404(Document, slug=slug)
+    return redirect(doc.file.url)
