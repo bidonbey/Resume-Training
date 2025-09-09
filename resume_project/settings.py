356bcacd42057c3cedf464d4d34ac7208632ebdc
BidonBey <bidonbey@domain.com>
2025-09-09 17:37:58 +0300

    BAckupv1

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from email.policy import default
 
 import environ
 import os
@@ -91,10 +92,7 @@ WSGI_APPLICATION = 'resume_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default":env.db()
 }
 
 
